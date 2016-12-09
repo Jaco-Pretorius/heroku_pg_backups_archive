4bf391516fca478a2f634a001d223ee959a3dd67
Brent Wheeldon <brent.wheeldon@gmail.com>
2016-12-09 10:38:37 -0500

    Update syntax to use new style.
    For details of the change, see [here](https://devcenter.heroku.com/changelog-items/1058).

--- lib/heroku_pg_backups_archive/toolbelt_helper.rb
+++ lib/heroku_pg_backups_archive/toolbelt_helper.rb
@@ -4,15 +4,15 @@ module HerokuPgBackupsArchive
 
     class << self
       def capture_backup
-        run("pg:backups capture -a #{HerokuPgBackupsArchive.config.app_name} #{follower_db(HerokuPgBackupsArchive.config.app_name)}")
+        run("pg:backups:capture -a #{HerokuPgBackupsArchive.config.app_name} #{follower_db(HerokuPgBackupsArchive.config.app_name)}")
       end
 
       def fetch_backup_public_url(backup_id)
-        run("pg:backups public-url #{backup_id} -q -a #{HerokuPgBackupsArchive.config.app_name}")
+        run("pg:backups:public-url #{backup_id} -a #{HerokuPgBackupsArchive.config.app_name}")
       end
 
       def fetch_backup_info(backup_id)
-        run("pg:backups info #{backup_id} -a #{HerokuPgBackupsArchive.config.app_name}")
+        run("pg:backups:info #{backup_id} -a #{HerokuPgBackupsArchive.config.app_name}")
       end
 
       private
